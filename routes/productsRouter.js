da5252f29fa2905c7f4e337e9449efd741db2231
Facundo Undiano <facundoundiano@gmail.com>
2023-08-06 16:22:20 -0300

    installing and requiring Multer

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
-const router = express.Router(); 
+const router = express.Router();
+const multer = require('multer');
 const productController = require('../controllers/productsController');
 
 
